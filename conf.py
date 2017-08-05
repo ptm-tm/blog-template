c339dea52ecd92d44208b4c79e2b98a662d0dc82
ptm-tm <kbshadow@yandex.ru>
2017-08-05 08:44:22 +0300

    Update conf.py
    STRIP_INDEXES = True

--- conf.py
+++ conf.py
@@ -936,7 +936,7 @@ COMMENT_SYSTEM_ID = "megahardlab"
 # (Uses the INDEX_FILE setting, so if that is, say, default.html,
 # it will instead /foo/default.html => /foo)
 # (Note: This was briefly STRIP_INDEX_HTML in v 5.4.3 and 5.4.4)
-STRIP_INDEXES = False
+STRIP_INDEXES = True
 
 # Should the sitemap list directories which only include other directories
 # and no files.
