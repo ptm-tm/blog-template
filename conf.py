886d80da33cba67976f23f31ccde40bed8d4119c
ptm-tm <kbshadow@yandex.ru>
2017-10-05 15:39:22 +0300

    libretto v2

--- conf.py
+++ conf.py
@@ -154,9 +154,9 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3"
+# THEME = "bootstrap3"
 # WTF?
-# THEME = "libretto"
+THEME = "libretto"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
