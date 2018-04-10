9066ac1dabf9a6059a45f8aa02dc441e16ee5ef8
kbshadow <kbshadow@yandex.ru>
2018-04-10 06:53:35 -0400

    conf update

--- conf.py
+++ conf.py
@@ -721,9 +721,8 @@ FILTERS = {
 # "source/" and the results will be located in
 # "OUTPUT_PATH/relative_destination/gallery_name"
 # Default is:
-GALLERY_FOLDERS = {"galleries": "galleries",
-                   "galleries/Pavlov":"Pavlov"
-                  }
+GALLERY_FOLDERS = {"galleries/": "galleries"}
+
 # More gallery options:
 THUMBNAIL_SIZE = 180
 # MAX_IMAGE_SIZE = 1280
