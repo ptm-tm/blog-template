53f044f1790825a32c4c97797cc19982aae61ab5
kbshadow <kbshadow@yandex.ru>
2018-04-11 02:59:15 -0400

    bootblog /v8

--- conf.py
+++ conf.py
@@ -161,7 +161,7 @@ NAVIGATION_LINKS = {
 # THEME = "bootstrap3"
 # WTF?
 #THEME = "bootstrap3"
-THEME = "bootblog4"
+THEME = "bootblog"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
